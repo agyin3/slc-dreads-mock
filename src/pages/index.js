71cb014b4c8626f317624e24216672e71290a508
Buddy Agyin <agyin3@gmail.com>
2020-09-04 13:08:12 -0600

    import services component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from '../components/hero'
+import Services from "../components/services/services"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero sitetitle='Salt Lake City Dreadlock Shop' />
+   <Services />
   </Layout>
 )
 
